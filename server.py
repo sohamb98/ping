722ae44b39bddd9f14ce1064d2f3f41fbe893d4d
thinkerxyz <bhattacharya.soham@outlook.com>
2019-04-14 19:02:20 +0530

    Client and Server

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+
 # first of all import the socket library 
 import socket                
   
@@ -7,7 +8,7 @@ print "Socket successfully created"
   
 # reserve a port on your computer in our 
 # case it is 12345 but it can be anything 
-port = 12345                
+port = 1221              
   
 # Next bind to the port 
 # we have not typed any ip in the ip field 
