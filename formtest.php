7bf991de8cf2486aca0a85f3a9d3aaad887f7af7
Soham Bhattacharya <bhattacharya.soham@outlook.com>
2019-04-15 10:04:42 +0530

    Main Objective Complete

--- formtest.php
+++ formtest.php
@@ -34,4 +34,16 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 $conn->close();
+
+$socket = stream_socket_client('tcp://'.$ip.':1220');
+if ($socket) {
+    $sent = stream_socket_sendto($socket, 'message');
+    if ($sent > 0) {
+        $server_response = fread($socket, 4096);
+        echo $server_response;
+    }
+} else {
+    echo 'Unable to connect to server';
+}
+stream_socket_shutdown($socket, STREAM_SHUT_RDWR);
 ?> 
